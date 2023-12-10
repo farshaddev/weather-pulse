2f8e45667c3b90849c290ae92ab5a06b0e5cb85b
farshad.development <farshad.development@gmail.com>
2023-12-10 14:25:42 +0330

    feat: add WhatsApp and Website

--- src/Components/Menu/MenuItems.tsx
+++ src/Components/Menu/MenuItems.tsx
@@ -3,6 +3,8 @@ import { BsGithub } from "react-icons/bs";
 import { FaLinkedinIn } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
 import { BsTelegram } from "react-icons/bs";
+import { FaEarthAsia } from "react-icons/fa6";
+import { RiWhatsappFill } from "react-icons/ri";
 
 export interface menuItemType {
 	icon: React.ReactNode;
@@ -12,6 +14,12 @@ export interface menuItemType {
 }
 
 export const menuItems: menuItemType[] = [
+	{
+		icon: <RiWhatsappFill />,
+		className: "dark:text-slate-300 flex-none",
+		text: "WhatsApp",
+		href: "https://wa.me/+989350696255",
+	},
 	{
 		icon: <BsTelegram />,
 		className: "text-socials-telegram-blue dark:text-slate-300 flex-none",
@@ -35,5 +43,11 @@ export const menuItems: menuItemType[] = [
 		className: "text-socials-gmail-red dark:text-slate-300 flex-none",
 		text: "Gmail",
 		href: "mailto:farshad.development@gmail.com",
+	},
+	{
+		icon: <FaEarthAsia />,
+		className: "dark:text-slate-300 flex-none",
+		text: "farshaddarvishi.com",
+		href: "https://farshaddarvishi.com",
 	},
 ];
